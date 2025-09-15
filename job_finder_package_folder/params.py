43a0e062b85062afb1fa8302ee2f2ad6e1e51c35
Thierry CAROLE <thcarole1@hotmail.com>
2025-09-15 17:47:53 +0200

    20250915_1747_Backup

--- job_finder_package_folder/params.py
+++ job_finder_package_folder/params.py
@@ -1,6 +1,11 @@
 import os
 
 ##################  VARIABLES  ##################
+# France Travail
 FT_CLIENT_ID = os.environ.get("FT_CLIENT_ID")
 FT_CLIENT_SECRET = os.environ.get("FT_CLIENT_SECRET")
 FT_SCOPE = os.environ.get("FT_SCOPE")
+
+# Adzuna
+ADZUNA_CLIENT_ID = os.environ.get("ADZUNA_CLIENT_ID")
+ADZUNA_CLIENT_SECRET = os.environ.get("ADZUNA_CLIENT_SECRET")
